d415109c6102551e14c70aea33bffd61db9c9b4d
Jaitun Godhani <jaitun.godhani@neenopal.com>
2025-05-22 16:35:14 +0530

    pass static token

--- src/services/cello.js
+++ src/services/cello.js
@@ -58,7 +58,7 @@ const initializeCelloInstance = async (userId, userDetails) => {
         // Get token from your backend
         console.log("Getting Cello token for userId:", userId);
         // const { token } = await getCelloToken(userId);
-        const token = 'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJwcm9kdWN0SWQiOiJzdGFnZS1hcHAubW9ra3VwLmFpIiwicHJvZHVjdFVzZXJJZCI6IjMifQ.mSxpWVUT5za2EUeUivCydyDvO97JeprVa1nswnC_nh5VoWFwWzT6imP28-KCf4rYvxDep_fwhP93-1wsetCTlw';
+        const token = 'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJwcm9kdWN0SWQiOiJzdGFnZS1hcHAubW9ra3VwLmFpIiwicHJvZHVjdFVzZXJJZCI6IjMiLCJpYXQiOjE3NDc5MTE4ODN9.C54B7oDcmvyS0XWMGdvmnninZVgBeg-XZrosFsYJloqbksgC1ObmFN5NgdlDFKFf0HzqwDUbk8yfh9iROG_PXw';
         console.log("Received token:", token);
 
         // Return a promise that resolves when Cello is initialized
